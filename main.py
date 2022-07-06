016950aaa3249d69bbf16deb76fbe87c79a20363
Happyface-Hacker <108838841+Happyface-Hacker@users.noreply.github.com>
2022-07-06 18:04:19 -0400

    main.py
    File for automated coffee barista.

---
+++ main.py
@@ -0,0 +1,9 @@
+#Coffee shop.
+#We're making a robot barista.
+
+print("Hello, welcome to Happyface Hackers \n" "Coffee!!!!!!!!!")
+
+
+name = input("What's your name? \n")
+
+print("Hello " + name + ", use this coffee on your journey from zero to hero")
