09b715c43ec80c3f803159fdedf338c0e73c1d67
Happyface-Hacker <108838841+Happyface-Hacker@users.noreply.github.com>
2022-07-14 13:47:35 -0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-#Coffee shop.
-#We're making a robot barista.
-
-print("Hello, welcome to Happyface Hackers \n" "Coffee!!!!!!!!!")
-
-
-name = input("What's your name? \n")
-
-print("Hello " + name + ", use this coffee on your journey from zero to hero")
